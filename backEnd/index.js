469bd955b1481348362df05b3df5bcfaf7011735
Хишигтогтох Nyamsuren <23LP6838@hishigee.local>
2024-02-23 19:39:56 +0800

    dfs

---
+++ backEnd/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+const app = express();
+app.use(cors());
+app.use(express.json());
+var arr = [];
+
+app.get("/", (request, response) => {
+  if (request) {
+    response.json(arr);
+  }
+});
+
+app.post("/", (request, response) => {
+  fs.readFileSync("db.json");
+  arr.push(request.body);
+  fs.writeFileSync("db.json", JSON.stringify(arr));
+  // arr = JSON.parse("db.json");
+  console.log(arr);
+  response.send(arr);
+});
+
+app.listen(3000, () => {
+  console.log(`3000 hello`);
+});
---
+++ frontEnd/src/pages/index.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [data, setData] = useState([]);
+  const createData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, age }),
+      });
+      const newData = await response.json();
+      setData(newData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  console.log(data);
+  const addData = async () => {
+    await createData();
+  };
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+  return (
+    <>
+      <div>
+        <p>Heloo</p>
+        <input
+          onChange={(event) => setName(event.target.value)}
+          placeholder="name"
+        ></input>
+        <input
+          onChange={(event) => setAge(event.target.value)}
+          placeholder="age"
+        ></input>
+        <button onClick={() => addData()}>submit</button>
+      </div>
+      {data?.map((el, index) => (
+        <div key={index} className="">
+          <div className="">{el.name}</div>
+          <div>{el.age}</div>
+        </div>
+      ))}
+    </>
+  );
+}
